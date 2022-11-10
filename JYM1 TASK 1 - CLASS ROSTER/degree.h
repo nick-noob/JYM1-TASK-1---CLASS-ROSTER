be97a84d85d2c65d721ccb92113d1fe9c5ecbb36
Nicholas Gusto <ngusto@wgu.edu>
2022-11-09 21:37:18 -0800

    Tightened up the comment area

--- JYM1 TASK 1 - CLASS ROSTER/degree.h
+++ JYM1 TASK 1 - CLASS ROSTER/degree.h
@@ -3,7 +3,6 @@
 /* 
 C.  Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE.
 
-
 Note: This information should be included in the degree.h file.
 */
 
