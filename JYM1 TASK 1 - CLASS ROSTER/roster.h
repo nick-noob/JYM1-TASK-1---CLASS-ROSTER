41299d8645705c17d570df85f2a5b1f51418bb85
Nicholas Gusto <ngusto@wgu.edu>
2022-11-26 18:38:25 -0800

    I thought that I was making good progress but I am not....still a long ways to go

--- JYM1 TASK 1 - CLASS ROSTER/roster.h
+++ JYM1 TASK 1 - CLASS ROSTER/roster.h
@@ -1,3 +1,36 @@
 #pragma once
 #include <student.h>
 
+using namespace std;
+
+const string studentData[] =
+
+{
+	"A1, John, Smith, John1989@gm ail.com, 20, 30, 35, 40, SECURITY",
+	"A2, Suzan, Erickson, Erickson_1990@gmailcom, 19, 50, 30, 40, NETWORK",
+	"A3, Jack, Napoli, The_lawyer99yahoo.com, 19, 20, 40, 33, SOFTWARE",
+	"A4, Erin, Black, Erin.black@comcast.net, 22, 50, 58, 40, SECURITY",
+	"A5, Nicholas, Gusto, Ngusto@wgu.edu, 31, 15, 25, 35, SOFTWARE"
+};
+
+class Roster {
+
+public: 
+	void add(string studentID, 
+			string firstName, 
+			string lastName,	
+			string studentEmail, 
+			int age, 
+			int daysToComplete[], 
+			degreeProgramEnum degreeProgram);
+
+	void remove(string studentID);
+	void printAll();
+	void printAverageDaysInCourse(string studentID);
+	void printInvalidEmails();
+	void printByDegreeProgram(DegreeProgram degreeProgram);
+
+	Roster();
+	~Roster();
+};
+
--- JYM1 TASK 1 - CLASS ROSTER/student.h
+++ JYM1 TASK 1 - CLASS ROSTER/student.h
@@ -1,10 +1,52 @@
 #pragma once
+#include <string>
 #include <degree.h>
+#include <vector>
+using namespace std;
 
 class Student {
 
-	string studentID, firstName, lastName, emailAddress;
-	int age, daysInCourse1, daysInCourse2, daysInCourse3;
-	DegreeProgram degreeProgram;
+private:
+
+	string studentID, firstName, lastName, studentEmail;
+	int age;
+	int daysToComplete[3];
+	degreeProgramEnum degreeProgram;
+	
+public:
+
+	// Let's access some functions!
+
+	string getStudentID() const;
+	string getFirstName() const;
+	string getLastName() const;
+	string getStudentEmail() const;
+	int getAge() const;
+	degreeProgramEnum getDegreeProgram() const;
+	int getDaysToComplete() const;
+
+	
+	// Let's mutate some functions!
+
+	void setStudentID(string studentID);
+	void setFirstName(string firstName);
+	void setLastName(string lastName);
+	void setStudentEmail(string studentEmail);
+	void setAge(int age);
+	void setDegreeProgram(degreeProgramEnum degreeProgram);
+	void setDaysToComplete(int daysToComplete);
+
+
+	// Time for some construction!
+
+	Student(
+		string studentID, string firstName, string lastName,
+		string studentEmail, int age, int daysToComplete[],
+		degreeProgramEnum degreeProgram);
+	
+	Student();
+	~Student();
+
+	virtual void print();
 
 };
